30856d5810fcef4696de8549fbeadd1a73a61a1e
pietvanzoen <pietvanzoen@gmail.com>
2017-05-09 16:58:30 +0200

    feat: setup processor

--- __tests__/lib/processor-test.js
+++ __tests__/lib/processor-test.js
@@ -1,5 +1,26 @@
-describe('thing', function() {
-  it('thing', function() {
-    expect(true).toEqual(true);
+const chance = require('chance')();
+
+const processor = require('../../lib/processor');
+
+describe('processor', function() {
+  beforeEach(function() {
+    this.mockData = [{}, {}];
+    this.mockTasks = [['zoo', jest.fn()], ['boo', jest.fn()]];
+  });
+
+  it('runs each given task on a list of data', function() {
+    processor(this.mockData, this.mockTasks);
+    expect(this.mockTasks[0][1]).toHaveBeenCalledWith(this.mockData);
+    expect(this.mockTasks[1][1]).toHaveBeenCalledWith(this.mockData);
+  });
+
+  it('returns an object made of given task keys and output', function() {
+    const zooMockOutput = chance.sentence();
+    const booMockOutput = chance.sentence();
+    this.mockTasks[0][1].mockReturnValue(zooMockOutput);
+    this.mockTasks[1][1].mockReturnValue(booMockOutput);
+    const result = processor(this.mockData, this.mockTasks);
+    expect(result.zoo).toBe(zooMockOutput);
+    expect(result.boo).toBe(booMockOutput);
   });
 });
