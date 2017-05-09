0423745a92265952b265c0d099483ca6c36ce831
pietvanzoen <pietvanzoen@gmail.com>
2017-05-09 18:03:59 +0200

    fix: add basic tests for averager

--- __tests__/lib/processor-test.js
+++ __tests__/lib/processor-test.js
@@ -5,22 +5,43 @@ const processor = require('../../lib/processor');
 describe('processor', function() {
   beforeEach(function() {
     this.mockData = [{}, {}];
-    this.mockTasks = [['zoo', jest.fn()], ['boo', jest.fn()]];
+    this.mockTasks = [
+      { name: 'zoo', func: jest.fn() },
+      { name: 'boo', func: jest.fn() }
+    ];
   });
 
   it('runs each given task on a list of data', function() {
     processor(this.mockData, this.mockTasks);
-    expect(this.mockTasks[0][1]).toHaveBeenCalledWith(this.mockData);
-    expect(this.mockTasks[1][1]).toHaveBeenCalledWith(this.mockData);
+    expect(this.mockTasks[0].func).toHaveBeenCalledWith(this.mockData);
+    expect(this.mockTasks[1].func).toHaveBeenCalledWith(this.mockData);
   });
 
   it('returns an object made of given task keys and output', function() {
     const zooMockOutput = chance.sentence();
     const booMockOutput = chance.sentence();
-    this.mockTasks[0][1].mockReturnValue(zooMockOutput);
-    this.mockTasks[1][1].mockReturnValue(booMockOutput);
+    this.mockTasks[0].func.mockReturnValue(zooMockOutput);
+    this.mockTasks[1].func.mockReturnValue(booMockOutput);
     const result = processor(this.mockData, this.mockTasks);
     expect(result.zoo).toBe(zooMockOutput);
     expect(result.boo).toBe(booMockOutput);
   });
+
+  it('uses default task list', function() {
+    const data = processor(this.mockData);
+    expect(typeof data).toBe('object');
+  });
 });
+
+describe('tasks', function() {
+  describe('avg', function() {
+    const avg = getTaskByName('avg');
+    it('returns some average', function() {
+      expect(avg.func()).toBe('some average');
+    });
+  });
+});
+
+function getTaskByName(name) {
+  return processor.TASKS.find(task => task.name === name);
+}
