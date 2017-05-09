238396105669dc6f4e3e8f8814691295a77de2bc
pietvanzoen <pietvanzoen@gmail.com>
2017-05-09 18:05:17 +0200

    feat: add basic index runner

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const Main = require('./lib/main');
+const FileService = require('./lib/file-service');
+const processor = require('./lib/processor');
 
+const main = new Main({
+  fileService: new FileService(),
+  processor: processor
+});
+
+console.log('processing...');
+main
+  .process(['./data/data1.json', './data/data2.json', './data/data3.json'])
+  .then(() => {
+    console.log('done!');
+  })
+  .catch(err => {
+    console.error(err);
+  });
